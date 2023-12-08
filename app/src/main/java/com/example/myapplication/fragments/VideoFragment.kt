0cdb2a81be2d2316902536ba97795d3aa5b30379
Mohd Ashfaq <tbeast305@gmail.com>
2023-12-08 09:09:14 +0530

    finished

--- app/src/main/java/com/example/myapplication/fragments/VideoFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/VideoFragment.kt
@@ -13,9 +13,9 @@ import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.FragmentVideoBinding
 
-class VideoFragment: Fragment() {
+class VideoFragment : Fragment() {
     private lateinit var binding: FragmentVideoBinding
-    var mediaControls: MediaController? = null
+    private lateinit var mediaControls: MediaController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,33 +29,31 @@ class VideoFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (mediaControls == null) {
-            // Creating an object of media controller class
-            mediaControls = MediaController(requireContext())
-            mediaControls!!.setAnchorView(binding.videoView)
-        }
+        // Path
+        binding.videoView.setVideoPath("android.resource://"+ requireActivity().packageName + "/" + R.raw.video1)
+        binding.videoView.start()
+
+        // Creating an object of media controller class
+        mediaControls = MediaController(requireContext())
+        mediaControls!!.setAnchorView(binding.videoView)
 
         // Set the media controller for video view
         binding.videoView.setMediaController(mediaControls)
 
-        // Path
-        binding.videoView.setVideoURI(
-            Uri.parse("android.resource://"
-                + "com.example.myapplication" + "/" + R.raw.video1))
-
-        binding.videoView.requestFocus()
-        binding.videoView.start()
-
         // Success Toast
         binding.videoView.setOnCompletionListener {
-            Toast.makeText(requireContext(), "Video completed",
-                Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                requireContext(), "Video completed",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
         // Error Toast
         binding.videoView.setOnErrorListener { _, _, _ ->
-            Toast.makeText(requireContext(), "An Error Occurred " +
-                    "While Playing Video !!!", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                requireContext(), "An Error Occurred " +
+                        "While Playing Video !!!", Toast.LENGTH_LONG
+            ).show()
             false
         }
     }
